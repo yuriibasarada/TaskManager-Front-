867d1790a1be5d7a0ff33131a35ae039b59676b5
yuriy <yuriyshkvarc@gmail.com>
2019-12-14 13:37:24 +0200

    to add a layouts

---
+++ src/layouts/EmptyLayout.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="empty-layout">
+    <router-view />
+  </div>
+</template>
+
+<script>
+import messages from '@/utils/messages'
+export default {
+  name: 'empty-layout',
+  mounted() {
+    console.log('empty')
+  },
+  computed: {
+    error() {
+      return this.$store.getters.error
+    }
+  },
+  watch: {
+    error(ServerError) {
+      this.$error(messages[ServerError] || 'Something went wrong.')
+    }
+  }
+}
+</script>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="main-layout">
+    <router-view />
+  </div>
+</template>
+<script>
+export default {
+  name: 'main-layout',
+  mounted() {
+    console.log('main')
+  },
+  computed: {
+    error() {
+      return this.$store.getters.error
+    }
+  },
+  watch: {
+    error(ServerError) {
+      this.$error(messages[ServerError] || 'Something went wrong.')
+    }
+  }
+}
+</script>
