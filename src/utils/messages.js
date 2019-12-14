c1118ce7c2acab7ac46a3c92e28d9036a90399a4
yuriy <yuriyshkvarc@gmail.com>
2019-12-14 13:38:34 +0200

    utils for project

---
+++ src/utils/messages.js
@@ -0,0 +1,8 @@
+export default {
+  logout: 'YouAreLoggedOut',
+  login: 'FirstLogin',
+  registered: 'SuccessfullyRegistered',
+  'User not found': 'NotFoundUserWithEmail',
+  'Bad Credentials': 'PasswordIncorrect',
+  'Email is already taken.': 'ThisEmailAlreadyUse'
+}
