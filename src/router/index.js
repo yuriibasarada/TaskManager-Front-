0fb6223da37ca7aea46f783fb52ef9cf16cad387
yuriy <yuriyshkvarc@gmail.com>
2019-12-14 13:37:35 +0200

    set up route

---
+++ src/router/index.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    meta: { layout: 'main' },
+    component: () => import('../views/Home.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    meta: { layout: 'empty' },
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/register',
+    name: 'register',
+    meta: { layout: 'empty' },
+    component: () => import('../views/Register.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+router.beforeEach((to, from, next) => {
+  const requireAuth = to.matched.some(record => record.meta.auth)
+
+  if (requireAuth && !localStorage.token) {
+    next('/login?message=login')
+  } else {
+    next()
+  }
+})
+export default router
