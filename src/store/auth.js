17cfc87f753476eecd27a114e11042b7c208ae87
yuriy <yuriyshkvarc@gmail.com>
2019-12-14 13:38:04 +0200

    to add a new files for store

---
+++ src/store/auth.js
@@ -0,0 +1,52 @@
+import axios from 'axios'
+axios.defaults.headers['Content-Type'] = 'application/json'
+
+export default {
+  actions: {
+    async login({ dispatch, commit }, { email, password }) {
+      await axios
+        .post(process.env.VUE_APP_SERVER + '/login', { email, password })
+        .then(function(response) {
+          commit('setToken', response.data.token)
+        })
+        .catch(function(e) {
+          commit('setError', e.response.data.error)
+          throw e
+        })
+    },
+    async register({ dispatch, commit }, { email, password, name }) {
+      await axios
+        .post(process.env.VUE_APP_SERVER + '/register', {
+          email,
+          password,
+          name
+        })
+        .then(function(response) {
+          commit('setToken', response.data.token)
+        })
+        .catch(function(e) {
+          commit('setError', e.response.data.errors[0])
+          throw e
+        })
+    },
+
+    async getUid({ commit }) {
+      let token = localStorage.token
+      return await axios
+        .post('http://localhost:8000/auth/getid', { token })
+        .then(function(response) {
+          return response.data.id
+        })
+        .catch(function(e) {
+          commit('clearToken')
+          commit('setError', e.response)
+          throw e
+        })
+    },
+
+    async logout({ commit, dispatch }) {
+      commit('clearToken')
+      commit('clearInfo')
+    }
+  }
+}
---
+++ src/store/info.js
@@ -0,0 +1,42 @@
+export default {
+  state: {
+    info: {}
+  },
+  mutations: {
+    setInfo(state, info) {
+      state.info = info
+    },
+    clearInfo(state) {
+      state.info = {}
+    }
+  },
+  actions: {
+    async fetchInfo({ dispatch, commit }) {
+      try {
+        const uid = await dispatch('getUid')
+        const info = await axios
+          .get(`http://localhost:8000/auth/getinfo/${uid}`)
+          .then(function(response) {
+            return response.data
+          })
+        commit('setInfo', info)
+      } catch (e) {}
+    },
+    async updateInfo({ commit, dispatch, getters }, toUpdate) {
+      const uid = await dispatch('getUid')
+      const updateData = { ...getters.info, ...toUpdate }
+      await axios
+        .put(`http://localhost:8000/info/${uid}`, updateData)
+        .then(function(response) {
+          commit('setInfo', updateData)
+        })
+        .catch(function(e) {
+          commit('setError', e)
+          throw e
+        })
+    }
+  },
+  getters: {
+    info: s => s.info
+  }
+}
